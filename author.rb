92f7b63cc3aab029fdc2613dc3dd88a233cecfd5
“Mineliegoma” <garlandegoma@gmail.com>
2020-07-20 14:06:08 -0400

    mini_challenge2

--- author.rb
+++ author.rb
@@ -1,1 +1,23 @@
+class Author
+    attr_accessor :name  , :posts
+        def initialize(name)
+            @name= name
+           #@@all<<self
+        end 
+        def books
+            Post.all.select {|book|book.author == self}
+        end 
+         def add_book(title)
+            book.author = self    #it goes to the song find the artist and associate it to itself. we are writting equal and double equal because we are defining it
+         end 
+            def write_book(title)
+                
+              new_book = Book.new(title)  
+              #binding.pry
 
+                 add_book(new_book)
+            end
+            def self.most_word
+            Book.all.count 
+            end 
+        end 
--- book.rb
+++ book.rb
@@ -1,1 +1,15 @@
+class Book
+    attr_accessor :author , :title 
+    attr_reader :name
+    @@all=[]
+   def initialize(title)
+        @name= name
+        @title= title
+        @@all<< self 
+    end     
+    def self.all
+        @@all
+    end 
+    
 
+end 
